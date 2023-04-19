e388b522f50084ec6d85da561c7588fcb71a83d3
Karthik <karthikbt22@gmail.com>
2023-04-19 21:06:22 +0530

    added all files

---
+++ lib/flutter_session_jwt.dart
@@ -0,0 +1,113 @@
+library flutter_session_jwt;
+
+import 'dart:convert';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+/// Flutter session management using JWT token.
+///
+/// Note: Make sure to save token using ```FlutterSessionJwt.saveToken("token here")``` before using other methods
+class FlutterSessionJwt {
+  static const _storage = FlutterSecureStorage();
+
+  static const _keyJwtToken = 'jwtToken';
+
+  ///Save JWT token with encryption
+  ///
+  ///It accepts ```String``` and saves the token with advanced encyption
+  ///
+  ///Keychain is used for iOS
+  ///
+  ///AES encryption is used for Android. AES secret key is encrypted with RSA and RSA key is stored in KeyStore
+  static Future saveToken(String jwtToken) async => await _storage.write(
+        key: _keyJwtToken,
+        value: jwtToken,
+      );
+
+  //To get jwt token from storage
+  static _getJwtToken() async => await _storage.read(key: _keyJwtToken);
+
+  ///Get payload from jwt token
+  ///
+  ///Returns Map<String, dynamic> of the payload object which is encryped in jwt token
+  ///
+  ///```Note:```
+  ///Make sure to save token using ```FlutterSessionJwt.saveToken("token here")``` method before using other methods
+  static Map<String, dynamic> getPayload() {
+    final token = _getJwtToken();
+    if (token == "" || token == null) {
+      throw const FormatException(
+          'Invalid token : Please save valid jwt token');
+    } else {
+      final splitTheToken =
+          token.toString().split("."); // Split the token by '.'
+      if (splitTheToken.length != 3) {
+        //after splitting if array length is not equal to 3, thow invalid token
+        throw const FormatException('Invalid token');
+      }
+      try {
+        final payloadBase64 = splitTheToken[1]; // Payload will be at index 1
+
+        // Normalize data to remove special characters, spaces and also validate that the length is correct (a multiple of four).
+        final normalizedPayload = base64.normalize(payloadBase64);
+
+        // Decode the normalized data to stringified object
+        final payloadString = utf8.decode(base64.decode(normalizedPayload));
+
+        // Parse the String to a Map<String, dynamic> using jsonDecode
+        final decodedPayload = jsonDecode(payloadString);
+
+        // Return the decoded payload as object/map
+        return decodedPayload;
+      } catch (error) {
+        throw const FormatException('Invalid payload');
+      }
+    }
+  }
+
+  /// Throws Invalid token if parameter is not a valid JWT token.
+  ///
+  /// returns ```true``` if token has expired else returns ```false```
+  ///
+  ///```Note:```
+  ///Make sure to save token using ```FlutterSessionJwt.saveToken("token here")``` method before using other methods
+  static bool isTokenExpired() {
+    final expirationDate = getExpirationDateTime();
+    if (expirationDate == null) {
+      return false;
+    }
+    // If current date is after the expiration date from token, then JWT token is expired
+    return DateTime.now().isAfter(expirationDate);
+  }
+
+  static DateTime? _getExpiryDate({required String param}) {
+    final decodedToken = getPayload();
+    final expiration = decodedToken[param] as int?;
+    if (expiration == null) {
+      return null;
+    }
+    return DateTime.fromMillisecondsSinceEpoch(expiration *
+        1000); //to convert milliseconds to valid date time format based on parameter
+  }
+
+  /// Returns DateTime of token expiry
+  ///
+  ///```Note:```
+  ///Make sure to save token using ```FlutterSessionJwt.saveToken("token here")``` method before using other methods
+  static DateTime? getExpirationDateTime() {
+    return _getExpiryDate(param: 'exp');
+  }
+
+  /// Returns token issued date (iat)
+  ///
+  /// Throws [FormatException] if parameter is not a valid JWT token.
+  ///
+  ///```Note:```
+  ///Make sure to save token using ```FlutterSessionJwt.saveToken("token here")``` method before using other methods
+  static Duration? getIssuedDateTime() {
+    final issuedAtDate = _getExpiryDate(param: 'iat');
+    if (issuedAtDate == null) {
+      return null;
+    }
+    return DateTime.now().difference(issuedAtDate); //return
+  }
+}
